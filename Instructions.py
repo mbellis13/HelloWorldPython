1c809d3fd71c07a685532fdbafd83e0d77a58b4e
mbellis13 <36673046+mbellis13@users.noreply.github.com>
2018-08-29 21:25:05 -0400

    Add files via upload

---
+++ Instructions.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 29, 2018
+
+@author: mellis
+'''
+
+'''
+Create a new Module which prints the following message:
+     Hello, world.  I have just submitted my first program in Github.
+'''
